f13f1688756119495e9f3fd413c94e8d1c254655
Juan Arana Fu <78535998+Fu-Jose@users.noreply.github.com>
2022-02-17 17:43:10 +0100

    add Cards, add Lists, uuid

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,45 @@ import "./App.css";
 import Home from "./pages/home";
 import ContextAPI from "./context";
 import { mockData } from "./data/mockdata";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const addCard = () => {
-    console.log("CARD");
-  };
-  const addList = () => {
-    console.log("LIST");
-  };
   const [data, setData] = useState(mockData);
+
+  const addCard = (text: string, listId: string) => {
+    const newCardId = uuid();
+    const newCard = {
+      id: newCardId,
+      text: text,
+    };
+    const list = data.lists[listId];
+    list.cards = [...list.cards, newCard];
+    setData({
+      ...data,
+      lists: {
+        ...data.lists,
+        cards: list.cards,
+        // Both should be equivalent
+        // [listId]: list,
+      },
+    });
+  };
+
+  const addList = (text: string) => {
+    const newListId = uuid();
+    setData({
+      lists: {
+        ...data.lists,
+        [newListId]: {
+          id: newListId,
+          title: text,
+          cards: [],
+        },
+      },
+      listsIds: [...data.listsIds, newListId],
+    });
+  };
+
   return (
     <ContextAPI.Provider value={{ addCard, addList, data }}>
       <div className="App">
